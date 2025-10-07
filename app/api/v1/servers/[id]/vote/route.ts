e104b40909275562ef209c8b4492bbe69b9af94c
HorizonCode <horizoncode88@gmail.com>
2025-10-07 21:38:07 +0200

    refactor: improve getClientIp func

--- app/api/v1/servers/[id]/vote/route.ts
+++ app/api/v1/servers/[id]/vote/route.ts
@@ -3,18 +3,14 @@ import { ipVotesTable, serversTable, votesTable } from "@/lib/db/schema";
 import { eq, and, gte } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
-function getClientIp(req: NextRequest): string {
-  let ip = "unknown";
-  let x_forwarded_for = req.headers.get('x-forwarded-for');
-  let x_real_ip = req.headers.get('x-real-ip');
+const validIpHeaders = ["cf-connecting-ip", "x-real-ip", "x-forwarded-for"];
 
-  if (x_forwarded_for) {
-    ip = x_forwarded_for.split(',')[0];
-  } else if (x_real_ip) {
-    ip = x_real_ip;
-  } 
-  
-  return ip;
+function getClientIp(req: NextRequest): string {
+  for (const header of validIpHeaders) {
+    const ip = req.headers.get(header);
+    if (ip) return ip;
+  }
+  return "unknown";
 }
 
 export async function POST(
@@ -48,62 +44,84 @@ export async function POST(
 
   const ipAddress = getClientIp(req);
   if (ipAddress === "unknown") {
-    return NextResponse.json({ error: "Unable to determine client IP" }, { status: 400 });
+    return NextResponse.json(
+      { error: "Unable to determine client IP" },
+      { status: 400 }
+    );
   }
 
   // check if the user id has voted for this server in the last 24 hours
   // or if the ip address has voted for any server in the last 24 hours
   const twentyFourHoursAgo = Date.now() - 24 * 60 * 60 * 1000;
-  const recentIpVote = (await database
-    .select()
-    .from(ipVotesTable)
-    .where(and(
-      eq(ipVotesTable.ipAddress, ipAddress),
-      gte(ipVotesTable.lastVoted, twentyFourHoursAgo)
-    ))
-    .limit(1))[0];
+  const recentIpVote = (
+    await database
+      .select()
+      .from(ipVotesTable)
+      .where(
+        and(
+          eq(ipVotesTable.ipAddress, ipAddress),
+          gte(ipVotesTable.lastVoted, twentyFourHoursAgo)
+        )
+      )
+      .limit(1)
+  )[0];
 
   if (recentIpVote) {
-    return NextResponse.json({ error: "You can only vote once every 24 hours" }, { status: 429 });
+    return NextResponse.json(
+      { error: "You can only vote once every 24 hours" },
+      { status: 429 }
+    );
   }
 
-  const recentUserVote = (await database
-    .select()
-    .from(votesTable)
-    .where(
-      and(
-        eq(votesTable.serverId, serverId),
-        eq(votesTable.userId, serverUserId),
-        gte(votesTable.lastVoted, twentyFourHoursAgo)
+  const recentUserVote = (
+    await database
+      .select()
+      .from(votesTable)
+      .where(
+        and(
+          eq(votesTable.serverId, serverId),
+          eq(votesTable.userId, serverUserId),
+          gte(votesTable.lastVoted, twentyFourHoursAgo)
+        )
       )
-    )
-    .limit(1)
+      .limit(1)
   )[0];
 
   if (recentUserVote) {
-    return NextResponse.json({ error: "You can only vote once every 24 hours" }, { status: 429 });
+    return NextResponse.json(
+      { error: "You can only vote once every 24 hours" },
+      { status: 429 }
+    );
   }
 
-  await database.insert(ipVotesTable).values({
-    ipAddress: ipAddress,
-    lastVoted: Date.now(),
-  }).onConflictDoUpdate({
-    target: ipVotesTable.ipAddress,
-    set: {
+  await database
+    .insert(ipVotesTable)
+    .values({
+      ipAddress: ipAddress,
       lastVoted: Date.now(),
-    },
-  }).run();
+    })
+    .onConflictDoUpdate({
+      target: ipVotesTable.ipAddress,
+      set: {
+        lastVoted: Date.now(),
+      },
+    })
+    .run();
 
-  await database.insert(votesTable).values({
-    serverId: serverId,
-    userId: serverUserId,
-    lastVoted: Date.now(),
-  }).onConflictDoUpdate({
-    target: [votesTable.serverId, votesTable.userId],
-    set: {
+  await database
+    .insert(votesTable)
+    .values({
+      serverId: serverId,
+      userId: serverUserId,
       lastVoted: Date.now(),
-    },
-  }).run();
+    })
+    .onConflictDoUpdate({
+      target: [votesTable.serverId, votesTable.userId],
+      set: {
+        lastVoted: Date.now(),
+      },
+    })
+    .run();
 
   const updatedServer = await database
     .update(serversTable)
@@ -113,7 +131,6 @@ export async function POST(
     .where(eq(serversTable.id, serverId))
     .returning()
     .get();
-
 
   // TODO: configurable vote url for the server that we can POST to
   return NextResponse.json(updatedServer);
